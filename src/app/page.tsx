4d6c367af4dc477ded3b761c0e80c37b4ef4d433
chaiyaphat20 <chaiyaphat.b5922314@gmail.com>
2023-10-29 02:26:28 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Typography } from '@mui/material'
+'use client'
+import { Theme, Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <div>
-      <Typography variant="h1">Responsive h1 Desktop First</Typography>
+      <Typography variant="h1" sx={(theme:Theme)=>({
+        backgroundColor: 'red',
+        [theme.breakpoints.down("desktop")]: {
+          backgroundColor: 'green',
+        },
+        [theme.breakpoints.down("laptop")]: {
+          backgroundColor: 'yellow',
+        },
+        [theme.breakpoints.down("tablet")]: {
+          backgroundColor: 'pink',
+        }
+        
+      })}>Responsive h1 Desktop First</Typography>
       <Typography variant="h2" >Responsive h2 Desktop First</Typography>
       <Typography variant="h3" >Responsive h3 Desktop First</Typography>
       <Typography variant="h4" >Responsive h4 Desktop First</Typography>
--- src/app/theme/TypographyTheme.tsx
+++ src/app/theme/TypographyTheme.tsx
@@ -4,52 +4,52 @@ export const createThemeTypography = (theme:Theme):Theme =>{
   //Desktop first screen
   theme.typography.h1 = {
     fontSize: "2.5rem",
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "2.25rem",
+    },
     [theme.breakpoints.down("laptop")]: {
       fontSize: "2rem",
     },
     [theme.breakpoints.down("tablet")]: {
-      fontSize: "1.5rem",
-    },
-    [theme.breakpoints.down("mobile")]: {
-      fontSize: "1rem",
+      fontSize: "1.75rem",
     },
   };
 
   theme.typography.h2 = {
     fontSize: "1.875rem",
-    [theme.breakpoints.down("laptop")]: {
+    [theme.breakpoints.down("desktop")]: {
       fontSize: "1.625rem",
     },
-    [theme.breakpoints.down("tablet")]: {
-      fontSize: "1.375",
+    [theme.breakpoints.down("laptop")]: {
+      fontSize: "1.375rem",
     },
-    [theme.breakpoints.down("mobile")]: {
-      fontSize: "1.125",
+    [theme.breakpoints.down("tablet")]: {
+      fontSize: "1.125rem",
     },
   };
 
   theme.typography.h3 = {
     fontSize: "1.5rem",
-    [theme.breakpoints.down("laptop")]: {
+    [theme.breakpoints.down("desktop")]: {
       fontSize: "1.25rem",
     },
-    [theme.breakpoints.down("tablet")]: {
+    [theme.breakpoints.down("laptop")]: {
       fontSize: "1rem",
     },
-    [theme.breakpoints.down("mobile")]: {
+    [theme.breakpoints.down("tablet")]: {
       fontSize: "0.75rem",
     },
   };
 
   theme.typography.h4 = {
     fontSize: "1.25rem",
-    [theme.breakpoints.down("laptop")]: {
+    [theme.breakpoints.down("desktop")]: {
       fontSize: "1rem",
     },
-    [theme.breakpoints.down("tablet")]: {
+    [theme.breakpoints.down("laptop")]: {
       fontSize: "0.75rem",
     },
-    [theme.breakpoints.down("mobile")]: {
+    [theme.breakpoints.down("tablet")]: {
       fontSize: "0.5rem",
     },
   };
