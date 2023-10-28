aad2d3db2c0f13de8139317a09c443f5fbe5bb8f
chaiyaphat20 <chaiyaphat.b5922314@gmail.com>
2023-10-29 02:06:58 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import { Typography } from '@mui/material'
 export default function Home() {
   return (
     <div>
-      <Typography variant="h1" sx={{backgroundColor:'mistyrose'}}>Responsive h1 Desktop First</Typography>
+      <Typography variant="h1">Responsive h1 Desktop First</Typography>
+      <Typography variant="h2" >Responsive h2 Desktop First</Typography>
+      <Typography variant="h3" >Responsive h3 Desktop First</Typography>
     </div>
   )
 }
--- src/app/theme/TypographyTheme.tsx
+++ src/app/theme/TypographyTheme.tsx
@@ -6,16 +6,43 @@ import { Kanit } from "next/font/google";
 export const createThemeTypography = (theme:Theme):Theme =>{
   //Desktop first screen
   theme.typography.h1 = {
-    fontSize: "5rem",
+    fontSize: "2.5rem",
     [theme.breakpoints.down("laptop")]: {
-      fontSize: "3rem",
+      fontSize: "2rem",
     },
     [theme.breakpoints.down("tablet")]: {
-      fontSize: "2rem",
+      fontSize: "1.5rem",
     },
     [theme.breakpoints.down("mobile")]: {
       fontSize: "1rem",
     },
   };
+
+  theme.typography.h2 = {
+    fontSize: "1.875rem",
+    [theme.breakpoints.down("laptop")]: {
+      fontSize: "1.625rem",
+    },
+    [theme.breakpoints.down("tablet")]: {
+      fontSize: "1.375",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1.125",
+    },
+  };
+
+  theme.typography.h3 = {
+    fontSize: "1.5rem",
+    [theme.breakpoints.down("laptop")]: {
+      fontSize: "1.25rem",
+    },
+    [theme.breakpoints.down("tablet")]: {
+      fontSize: "1rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "0.75rem",
+    },
+  };
+
   return theme
 }
